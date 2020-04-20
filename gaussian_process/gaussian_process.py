63bd2d3a62079548cbbfbbad87e65d8e7d81ae42
BigTheAndo <yuki1173316@yahoo.co.jp>
2020-04-20 22:13:38 +0900

    平均と分散を考慮してガウス分布から予測値を生成

--- gaussian_process/gaussian_process.py
+++ gaussian_process/gaussian_process.py
@@ -1,30 +1,42 @@
 import numpy as np
 from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 if __name__ == "__main__":
     n_samples = 10
-    n_features = 1
+    n_features = 2
     noise = 30
     bias = 1
     X, Y = make_regression(n_samples=n_samples,
                            n_features=n_features,
                            noise=noise,
                            bias=bias)
-                           
-    X_mean = X.mean()
-    X_std = X.std()
-    X = (X - X.mean()) / X.std()
+    X_mean = X.mean(axis=0)
+    X_std = X.std(axis=0)
+    X = (X - X_mean) / X_std
     Y = (Y - Y.mean()) / Y.std()
     Y = Y.reshape(n_samples, 1)
+    # 線形カーネル
     XXT = X.dot(X.T)
+    XXT_inv = np.linalg.inv(XXT)
 
-    X_test = np.random.rand()
+    X_test = np.random.rand(2) * 3
     X_test = (X_test - X_mean) / X_std
-    X_test = np.array([X_test])
     k_aster = X_test.dot(X.T)
-    Y_hat = k_aster.dot(XXT).dot(Y)
-
-    plt.scatter(X, Y)
-    plt.scatter(X_test, Y_hat)
+    k_asteraster = X_test.dot(X_test)
+    # P(Y)がしたがうガウス分布の平均
+    Y_mean = k_aster.dot(XXT_inv).dot(Y)
+    # P(Y)がしたがうガウス分布の分散
+    Y_var = k_asteraster - k_aster.T.dot(XXT_inv).dot(k_aster)
+    # 予測結果
+    result = np.random.normal(Y_mean[0], Y_var)
+    
+    """
+    描画部
+    """
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X[:, 0], X[:, 1], Y[:, 0])
+    ax.scatter(X_test[0], X_test[1], result)
     plt.show()
