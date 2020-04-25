26a02f4ac2cef55375516f2f7abc16045647f6e2
bigtheando <yuki1173316@yahoo.co.jp>
2020-04-25 14:57:15 +0900

    正則化を追加してクラス化

--- linear_regression/multiple_linear_regression.py
+++ linear_regression/multiple_linear_regression.py
@@ -3,6 +3,23 @@ from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+class MultipleLinearRegression:
+    def __init__(self, X, y, l2_coeff=1E-3):
+        assert len(X) == len(y), "len(X) is not equal to len(y)"
+        self.X = X
+        self.y = y
+        self.W = None
+        # 正則化項
+        self.l2_coeff = l2_coeff
+    
+    def fit(self):
+        A = self.X.T.dot(self.X)
+        self.W = np.linalg.inv(A + self.l2_coeff * np.identity(len(A))).dot(self.X.T).dot(self.y)
+    
+    def predict(self, X):
+        return X.dot(self.W)
+
+
 if __name__ == "__main__":
     # data作成
     X_len = 50
@@ -14,17 +31,21 @@ if __name__ == "__main__":
     X = (X - X.mean(axis=0)) / X.std(axis=0)
     y = (y - y.mean(axis=0)) / y.std(axis=0)
     y = y.reshape(X_len, 1)
-    A = X.T.dot(X)
-    w = np.linalg.inv(A).dot(X.T).dot(y)
-    
-    x_hat = np.arange(-5, 5).reshape(10, 1)
-    y_hat = np.arange(-5, 5).reshape(10, 1)
-    z_hat = np.hstack((x_hat, y_hat)).dot(w)
-    x_hat = x_hat.flatten()
-    y_hat = y_hat.flatten()
-    z_hat = z_hat.flatten()
+
+    model = MultipleLinearRegression(X=X, y=y, l2_coeff=1E-3)
+    model.fit()
+
+    # 推論    
+    X_hat = np.random.rand(10, 2)
+    y_hat = model.predict(X=X_hat)
+
+    """
+    描画部
+    """
     fig = plt.figure()
     ax = Axes3D(fig)
-    ax.plot_surface(np.ravel(x_hat), np.ravel(y_hat), np.ravel(z_hat), alpha=0.3)
+    # 青点が学習用データ
     ax.scatter(X[:, 0], X[:, 1], zs=y)
+    # 橙点が推論結果
+    ax.scatter(X_hat[:, 0], X_hat[:, 1], y_hat)
     plt.show()
